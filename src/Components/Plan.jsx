e21beb416459ae12eb176798b72a8945b9ea956d
Ashish Agnihotri <agnih96@gmail.com>
2022-10-01 01:08:46 +0530

    day1

---
+++ src/Components/Plan.jsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Plan = () => {
+  return (
+    <Box mt="10rem">
+        <Text fontWeight="bold" color="#749fce" fontSize="20px">Plan Comparison</Text>
+    </Box>
+  )
+}
+
+export default Plan
